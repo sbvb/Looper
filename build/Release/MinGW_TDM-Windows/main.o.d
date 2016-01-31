f161084fef53394050d21f3b420da3a9250c59f1
Carlos Eduardo Marciano <carloseduardov8@hotmail.com>
2016-01-31 17:30:14 -0200

    Início do projeto
    Arquivos iniciais, primeiro upload.

---
+++ build/Release/MinGW_TDM-Windows/main.o.d
@@ -0,0 +1,8 @@
+build/Release/MinGW_TDM-Windows/main.o: main.cpp libportaudio/portaudio.h \
+ pa_win_coinitialize.h portaudio.h
+
+libportaudio/portaudio.h:
+
+pa_win_coinitialize.h:
+
+portaudio.h:
