e755e773594e957061d7d7ff804bcc565e4ba489
carloseduardov8 <carloseduardov8@hotmail.com>
2016-02-01 21:30:49 -0200

    Looper mais avancado
    Gravar mais de uma track e consegue dar playback enquanto grava a
    proxima.

--- main.cpp
+++ main.cpp
@@ -41,14 +41,35 @@
  * license above.
  */
 
+#ifdef _WIN32
+    #include <windows.h>
+
+    void sleep(unsigned milliseconds)
+    {
+        Sleep(milliseconds);
+    }
+#else
+    #include <unistd.h>
+
+    void sleep(unsigned milliseconds)
+    {
+        usleep(milliseconds * 1000); // takes microseconds
+    }
+#endif
+
+
+
+using namespace std;
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 #include "portaudio.h"
 
 /* #define SAMPLE_RATE  (17932) // Test failure to open with this value. */
 #define SAMPLE_RATE  (44100)
 #define FRAMES_PER_BUFFER (512)
-#define NUM_SECONDS     (5)
+#define NUM_SECONDS     (10)
 #define NUM_CHANNELS    (2)
 /* #define DITHER_FLAG     (paDitherOff) */
 #define DITHER_FLAG     (0) /**/
@@ -140,6 +161,42 @@ static int recordCallback( const void *inputBuffer, void *outputBuffer,
     return finished;
 }
 
+char* stringMultiplier(char multiplying, int multiplicator){ //This function forms strings composed of the same character.
+    int i;
+    static char r[100];
+    for (i = 0; i<100; i++){
+        r[i] = '\0';
+    }
+    if (multiplying <= 0){
+        return r;
+    } else {
+        for (i=0; i<multiplicator; i++){
+            r[i] = multiplying;
+        }
+    }
+    return r;
+}
+
+void invokeWaitBeeps(int beeps, int totalMiliseconds, char mode){
+    int k = beeps;
+    int temp;
+    int miliseconds = totalMiliseconds/beeps;
+    cout << "|*" << stringMultiplier(' ', k-1) << "|" << ((mode == 'p') ? " <-RECORD HERE" : "  RECORDING...") << flush;
+    sleep(miliseconds);
+    for (k = beeps; k>1; k--){
+        temp = k;
+        temp = k+14;
+        cout << stringMultiplier('\b', temp) << "*" << flush;
+        cout << stringMultiplier(' ', k-2) << "|" << ((mode == 'p') ? " <-RECORD HERE" : "  RECORDING...")<< flush;
+        sleep(miliseconds);
+    }
+    if (mode == 'p') {
+        cout << stringMultiplier('\b', beeps+16) << flush;
+    } else if (mode == 'r'){
+        cout << stringMultiplier('\b', 12) << "    DONE!   " << flush;
+    }
+}
+
 /* This routine will be called by the PortAudio engine when audio is needed.
 ** It may be called at interrupt level on some machines so don't do anything
 ** that could mess up the system like calling malloc() or free().
@@ -173,7 +230,6 @@ static int playCallback( const void *inputBuffer, void *outputBuffer,
         
         data->frameIndex += framesLeft;
         if (framesLeft <= 100){
-            printf("Hello moma");
             data->frameIndex = 0;
         }
         finished = paContinue;
@@ -197,35 +253,20 @@ int main(void)
 {
     PaStreamParameters  inputParameters,
                         outputParameters;
-    PaStream*           stream;
-    PaStream*           stream2;
+    PaStream*           stream[6];
     PaError             err = paNoError;
-    paTestData          data;
-    paTestData          data2;
+    paTestData          data[6];
     int                 i;
+    int                 k;
     int                 totalFrames;
     int                 numSamples;
     int                 numBytes;
     SAMPLE              max, val;
     double              average;
-
-    printf("patest_record.c\n"); fflush(stdout);
-
-    data.maxFrameIndex = totalFrames = NUM_SECONDS * SAMPLE_RATE; /* Record for a few seconds. */
-    data.frameIndex = 0;
-    data2.maxFrameIndex = totalFrames = NUM_SECONDS * SAMPLE_RATE; /* Record for a few seconds. */
-    data2.frameIndex = 0;
-    numSamples = totalFrames * NUM_CHANNELS;
-    numBytes = numSamples * sizeof(SAMPLE);
-    data.recordedSamples = (SAMPLE *) malloc( numBytes ); /* From now on, recordedSamples is initialised. */
-    data2.recordedSamples = (SAMPLE *) malloc( numBytes ); /* From now on, recordedSamples is initialised. */
-    if( data.recordedSamples == NULL )
-    {
-        printf("Could not allocate record array.\n");
-        goto done;
-    }
-    for( i=0; i<numSamples; i++ ) data.recordedSamples[i] = 0;
-    for( i=0; i<numSamples; i++ ) data2.recordedSamples[i] = 0;
+    
+    cout << "AUDIO LOOPER" << endl << endl << "Press 'r' when you wish to record a track. You may record up to 6 tracks." << endl << "When you do, pay close attention to the metronome and start recording when it reaches its end." << endl;
+    
+    
 
     err = Pa_Initialize();
     if( err != paNoError ) goto done;
@@ -241,180 +282,97 @@ int main(void)
     inputParameters.hostApiSpecificStreamInfo = NULL;
 
     /* Record some audio. -------------------------------------------- */
-    err = Pa_OpenStream(
-              &stream,
-              &inputParameters,
-              NULL,                  /* &outputParameters, */
-              SAMPLE_RATE,
-              FRAMES_PER_BUFFER,
-              paClipOff,      /* we won't output out of range samples so don't bother clipping them */
-              recordCallback,
-              &data );
-    if( err != paNoError ) goto done;
-
-    err = Pa_StartStream( stream );
-    if( err != paNoError ) goto done;
-    printf("\n=== Now recording!! Please speak into the microphone. ===\n"); fflush(stdout);
-
-    while( ( err = Pa_IsStreamActive( stream ) ) == 1 )
-    {
-        Pa_Sleep(1000);
-        printf("index = %d\n", data.frameIndex ); fflush(stdout);
-    }
-    if( err < 0 ) goto done;
-
-    err = Pa_CloseStream( stream );
-    if( err != paNoError ) goto done;
-
-    /* Measure maximum peak amplitude. */
-    max = 0;
-    average = 0.0;
-    for( i=0; i<numSamples; i++ )
-    {
-        val = data.recordedSamples[i];
-        if( val < 0 ) val = -val; /* ABS */
-        if( val > max )
-        {
-            max = val;
-        }
-        average += val;
-    }
-
-    average = average / (double)numSamples;
-
-    printf("sample max amplitude = "PRINTF_S_FORMAT"\n", max );
-    printf("sample average = %lf\n", average );
-    /* Segunda gravacao:*/
     
-    err = Pa_OpenStream(
-              &stream,
-              &inputParameters,
-              NULL,                  /* &outputParameters, */
-              SAMPLE_RATE,
-              FRAMES_PER_BUFFER,
-              paClipOff,      /* we won't output out of range samples so don't bother clipping them */
-              recordCallback,
-              &data2 );
-    if( err != paNoError ) goto done;
-
-    err = Pa_StartStream( stream );
-    if( err != paNoError ) goto done;
-    printf("\n=== Now recording!! Please speak into the microphone. ===\n"); fflush(stdout);
-
-    while( ( err = Pa_IsStreamActive( stream ) ) == 1 )
-    {
-        Pa_Sleep(1000);
-        printf("index = %d\n", data2.frameIndex ); fflush(stdout);
-    }
-    if( err < 0 ) goto done;
-
-    err = Pa_CloseStream( stream );
-    if( err != paNoError ) goto done;
-
-    /* Measure maximum peak amplitude. */
-    max = 0;
-    average = 0.0;
-    for( i=0; i<numSamples; i++ )
-    {
-        val = data2.recordedSamples[i];
-        if( val < 0 ) val = -val; /* ABS */
-        if( val > max )
-        {
-            max = val;
+    k = 0;
+    do{
+        
+        data[k].maxFrameIndex = totalFrames = NUM_SECONDS * SAMPLE_RATE; /* Record for a few seconds. */
+        data[k].frameIndex = 0;
+        
+        numSamples = totalFrames * NUM_CHANNELS;
+        numBytes = numSamples * sizeof(SAMPLE);
+        
+        data[k].recordedSamples = (SAMPLE *) malloc( numBytes ); /* From now on, recordedSamples is initialised. */
+        if( data[k].recordedSamples == NULL ) {
+            cout << "Could not allocate record array." << endl << flush;
+            goto done;
         }
-        average += val;
-    }
-
-    average = average / (double)numSamples;
-
-    printf("sample max amplitude = "PRINTF_S_FORMAT"\n", max );
-    printf("sample average = %lf\n", average );
-
-    /* Write recorded data to a file. */
-#if WRITE_TO_FILE
-    {
-        FILE  *fid;
-        fid = fopen("recorded.raw", "wb");
-        if( fid == NULL )
-        {
-            printf("Could not open file.");
+        for( i=0; i<numSamples; i++ ) {
+            data[k].recordedSamples[i] = 0;
         }
-        else
-        {
-            fwrite( data.recordedSamples, NUM_CHANNELS * sizeof(SAMPLE), totalFrames, fid );
-            fclose( fid );
-            printf("Wrote data to 'recorded.raw'\n");
+ 
+        err = Pa_OpenStream(
+                  &stream[k],
+                  &inputParameters,
+                  NULL,                  /* &outputParameters, */
+                  SAMPLE_RATE,
+                  FRAMES_PER_BUFFER,
+                  paClipOff,      /* we won't output out of range samples so don't bother clipping them */
+                  recordCallback,
+                  &data[k] );
+        if( err != paNoError ) goto done;
+
+        while (1) {        /* Waits until the user presses r*/
+            if (getchar() == 'r'){
+                break;
+            }
         }
-    }
-#endif
-    /* Playback recorded data.  -------------------------------------------- */
-    data.frameIndex = 0;
-    data2.frameIndex = 0;
+        
+        cout << endl << flush;    /* Calls invokeWaitBeeps in pre-recording mode*/
+        invokeWaitBeeps(8, NUM_SECONDS*500, 'p');
 
-    outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
-    if (outputParameters.device == paNoDevice) {
-        fprintf(stderr,"Error: No default output device.\n");
-        goto done;
-    }
-    outputParameters.channelCount = 2;                     /* stereo output */
-    outputParameters.sampleFormat =  PA_SAMPLE_TYPE;
-    outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
-    outputParameters.hostApiSpecificStreamInfo = NULL;
 
-    printf("\n=== Now playing back. ===\n"); fflush(stdout);
+        err = Pa_StartStream( stream[k] );     /*Starts recording*/
+        if( err != paNoError ) goto done;
+
+
+        invokeWaitBeeps(8, NUM_SECONDS*1000, 'r');     /* Calls invokeWaitBeeps in recording mode */
+
+
+
+        err = Pa_CloseStream( stream[k] );    /*Closes the recording stream*/
+        if( err != paNoError ) goto done;
+        
+        /* Playback recorded data.  -------------------------------------------- */
+        
+        data[k].frameIndex = 0;
+
+        outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
+        if (outputParameters.device == paNoDevice) {
+            fprintf(stderr,"Error: No default output device.\n");
+            goto done;
+        }
+        outputParameters.channelCount = 2;                     /* stereo output */
+        outputParameters.sampleFormat =  PA_SAMPLE_TYPE;
+        outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
+        outputParameters.hostApiSpecificStreamInfo = NULL;
+
+        err = Pa_OpenStream(
+                  &stream[k],
+                  NULL, /* no input */
+                  &outputParameters,
+                  SAMPLE_RATE,
+                  FRAMES_PER_BUFFER,
+                  paClipOff,      /* we won't output out of range samples so don't bother clipping them */
+                  playCallback,
+                  &data[k] );
+        if( err != paNoError ) goto done;
+
+
+        err = Pa_StartStream( stream[k] );
+        if( err != paNoError ) goto done;
     
-    err = Pa_OpenStream(
-              &stream,
-              NULL, /* no input */
-              &outputParameters,
-              SAMPLE_RATE,
-              FRAMES_PER_BUFFER,
-              paClipOff,      /* we won't output out of range samples so don't bother clipping them */
-              playCallback,
-              &data );
-    if( err != paNoError ) goto done;
-    
-    err = Pa_OpenStream(
-              &stream2,
-              NULL, /* no input */
-              &outputParameters,
-              SAMPLE_RATE,
-              FRAMES_PER_BUFFER,
-              paClipOff,      /* we won't output out of range samples so don't bother clipping them */
-              playCallback,
-              &data2 );
-    if( err != paNoError ) goto done;
-
-    if( stream )
-    {
-        err = Pa_StartStream( stream );
-        if( err != paNoError ) goto done;
-        
-
-        
-        err = Pa_StartStream( stream2 );
-        if( err != paNoError ) goto done;
-        
-        printf("Waiting for playback to finish.\n"); fflush(stdout);
-        
-       
-        
-        while((err = Pa_IsStreamActive(stream2)) == 1) Pa_Sleep(100);
-        
-        if( err < 0 ) goto done;
-        
-        err = Pa_CloseStream( stream );
-        if( err != paNoError ) goto done;
-        err = Pa_CloseStream( stream2 );
-        if( err != paNoError ) goto done;
-        
-        printf("Done.\n"); fflush(stdout);
-    }
+        ++k;
+    } while (k < 6);
+    cout << endl << endl << "All 6 tracks are now playing." << flush;
+    while (1) sleep(10000);
 
 done:
     Pa_Terminate();
-    if( data.recordedSamples )       /* Sure it is NULL or valid. */
-        free( data.recordedSamples );
+    for (k = 0; k < 2; k++){
+        if( data[k].recordedSamples )       /* Sure it is NULL or valid. */
+            free( data[k].recordedSamples );
+    }
     if( err != paNoError )
     {
         fprintf( stderr, "An error occured while using the portaudio stream\n" );
